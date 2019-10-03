d1edfe7d92e67bda68c85019b4d9a3cd0fecee55
Livia <lqoshe@princeton.edu>
2019-10-03 16:09:52 -0400

    Working final

--- pset3.jl
+++ pset3.jl
@@ -63,3 +63,68 @@ subplot(1,2,2)
 imshow(image2);
 axis("off")
 title("El Capitan: GBR shifted")
+
+
+# Part B
+
+"""
+circularRotation() -- Pixel shifts (rotates) a color channel in imagefile
+circularRotation(): Shifts the pixels of a specified color channel (red, green,
+    or blue) in the image specified by imagefile, up by the numRotations
+    specified. 
+
+Args:
+   imagefile (string): Name of image file. (must be in current wd)
+   numRotations (int): number of pixels to rotate up
+   channel (string): which color channel to rotate up? (must be red, green, or blue)
+
+Returns:
+   newimage Array{Float32,3}: the newly rotated image. Has the same dimensions
+            as the image corresponding to imagefile.
+"""
+function circularRotation(imagefile, numRotations, channel)
+
+    imagename = imread(imagefile);
+    newimage = imread(imagefile);
+
+    if channel == "red"
+        dimension3 = 1;
+    end
+
+    if channel == "green"
+        dimension3 = 1;
+    end
+
+    if channel == "blue"
+        dimension3 = 1;
+    end
+
+    numrows = size(imagename)[1]
+    println(numrows)
+    # for each row, shift it down by numRotations
+    for i = 1: numrows
+        shiftRow = (i+numRotations) % numrows;
+
+        # special case for shifting last row, since no 0th row
+        if shiftRow == 0
+            shiftRow = numrows;
+        end
+        newimage[i, :, dimension3] = imagename[shiftRow, :, dimension3]
+    end
+
+    return newimage
+end
+
+rotated = circularRotation("el-capitan.png",180, "red");
+
+clf();
+
+subplot(1,2,1)
+imshow(image);
+axis("off")
+title("El Capitan: Original")
+
+subplot(1,2,2)
+imshow(rotated);
+axis("off")
+title("El Capitan: Red shift up 180p")
